21863b3123ca0f71ffb5767020c8b722ed379fed
matthew-weigand <matthew.weigand@bluestem.com>
2025-12-02 23:13:54 -0600

    -update github file

--- src/environments/environments.ts
+++ src/environments/environments.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiBaseUrl: 'https://mjw-web-resume-app-b5h8cabyczcwh0g7.centralus-01.azurewebsites.net', // change to your real URL
+  apiBaseUrl: 'https://mjw-web-resume-app-b5h8cabyczcwh0g7.centralus-01.azurewebsites.net/', // change to your real URL
 };
